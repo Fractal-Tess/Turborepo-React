9c3cbd0db5eb5acbe5aee82014f8178d5c1ca03f
Fractal-Tess <vgfractal@gmail.com>
2022-09-28 08:28:21 +0300

    first commit

---
+++ packages/ui/components/animation/AnimateInView.tsx
@@ -0,0 +1,56 @@
+import {
+  type Transition,
+  type Variant,
+  motion,
+  useAnimation,
+  useInView,
+} from 'framer-motion';
+import { useRef, useEffect, PropsWithChildren } from 'react';
+
+type Variants = {
+  visible: Variant;
+  hidden: Variant;
+};
+
+type Props = {
+  variants?: Variants;
+  transition?: Transition;
+} & PropsWithChildren;
+
+const defaultVariants: Variants = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
+
+const defaultTransition: Transition = {
+  ease: 'easeOut',
+  duration: 1.5,
+  delay: 0.4,
+};
+
+export default function GenericAnimation({
+  children,
+  variants = defaultVariants,
+  transition = defaultTransition,
+}: Props) {
+  const controls = useAnimation();
+
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref);
+
+  useEffect(() => {
+    if (isInView) controls.start('visible');
+  }, [controls, isInView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={variants}
+      transition={transition}
+      animate={controls}
+      initial="hidden"
+    >
+      {children}
+    </motion.div>
+  );
+}

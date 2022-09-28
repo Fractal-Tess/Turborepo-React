36e34db6cf224f2b5d7dfdbfedc5b58f5041471d
Fractal-Tess <vgfractal@gmail.com>
2022-09-28 19:58:37 +0300

    fix outlet

--- apps/react/src/layout/BaseLayout.tsx
+++ apps/react/src/layout/BaseLayout.tsx
@@ -2,13 +2,14 @@ import { type PropsWithChildren, useContext } from 'react';
 import ThemeWrapper from '$layout/ThemeWrapper';
 import Header from './Header';
 import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
 export default function BaseLayout({ children: page }: PropsWithChildren) {
   return (
     <ThemeWrapper>
       <div className="bg-gradient-to-t from-base-300 to-base-100 min-h-screen flex flex-col justify-between">
         <Header />
-        {page}
+        <Outlet />
         <Footer />
       </div>
     </ThemeWrapper>

8b46e40cb0b542fb71bb59ac82f784e476b7a9d6
Fractal-Tess <vgfractal@gmail.com>
2022-09-29 12:24:49 +0300

    removed comment

--- packages/tailwind-config/tailwind.config.cjs
+++ packages/tailwind-config/tailwind.config.cjs
@@ -9,46 +9,45 @@ const config = {
   content: [
     `${ui_package_path}/components/**/*.{html,js,ts,tsx}`,
     'src/**/*.{html,js,ts,tsx}',
-    'pages/**/*.{html,js,ts,tsx}',
+    'pages/**/*.{html,js,ts,tsx}'
   ],
   darkMode: 'class',
 
   theme: {
     extend: {
       fontFamily: {
-        sans: ['roboto', ...defaultTheme.fontFamily.sans],
+        sans: ['roboto', ...defaultTheme.fontFamily.sans]
       },
       boxShadow: {
-        'top-xl': '0 -20px 25px -5px rgba(0, 0,  0,  .1)',
+        'top-xl': '0 -20px 25px -5px rgba(0, 0,  0,  .1)'
       },
-      // TODO: Make this more random and subtle
       keyframes: {
         'gradient-keyframes-x': {
           '0%': { 'background-position': 'left' },
-          '100%': { 'background-position': 'right' },
-        },
+          '100%': { 'background-position': 'right' }
+        }
       },
       animation: {
         'gradient-animation-x':
           'gradient-keyframes-x 5s linear infinite alternate',
-        'spin-slow': 'spin 3s linear infinite',
-      },
-    },
+        'spin-slow': 'spin 3s linear infinite'
+      }
+    }
   },
 
   daisyui: {
     themes: [
       {
         dark: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]']
         },
         light: {
-          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
-        },
-      },
-    ],
+          ...require('daisyui/src/colors/themes')['[data-theme=light]']
+        }
+      }
+    ]
   },
-  plugins: [daisyui],
+  plugins: [daisyui]
 };
 
 module.exports = config;
